866049a1e95a5934b4f9b0fd6994a9d31af57a29
Dmitry_Gorshkov <libafer99@gmail.com>
2024-06-20 22:58:19 +0500

    feat: создана первая страница поиска

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,12 +1,55 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h1>SearchPage</h1>
+  <q-page class="q-pa-lg column justify-end gap-lg">
+    <q-field
+      class="col-12"
+      label="Откуда"
+    >
+      <template v-slot:prepend>
+        <q-icon
+          name="eva-pin-outline"
+          size="1.3rem"
+        />
+      </template>
+    </q-field>
+
+    <q-field label="Куда">
+      <template v-slot:prepend>
+        <q-icon
+          name="eva-pin-outline"
+          size="1.3rem"
+        />
+      </template>
+    </q-field>
+
+    <div class="row q-col-gutter-md">
+      <div class="col">
+        <q-field label="Дата">
+          <template v-slot:prepend>
+            <q-icon
+              name="eva-calendar-outline"
+              size="1.3rem"
+            />
+          </template>
+        </q-field>
+      </div>
+      <div class="col">
+        <q-field label="Пассажиры">
+          <template v-slot:prepend>
+            <q-icon
+              name="eva-people-outline"
+              size="1.3rem"
+            />
+          </template>
+        </q-field>
+      </div>
+    </div>
+
+    <q-btn
+      unelevated
+      color="primary"
+      >Поиск</q-btn
+    >
   </q-page>
 </template>
 
-<script setup lang="ts">
-
-defineOptions({
-  name: 'SearchPage'
-});
-</script>
+<script setup lang="ts"></script>
