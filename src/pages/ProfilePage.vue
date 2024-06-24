f2dee6d7d749005d660dcf6be7616a47348bf467
Dmitry_Gorshkov <libafer99@gmail.com>
2024-06-24 19:33:23 +0500

    feat: первый экран страницы профиля

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,8 +1,90 @@
+<script setup lang="ts">
+const extraLinks = ['Ваши отзывы', 'О проекте', 'Помощь', 'Оцените сервис', 'Пользовательское соглашение'];
+</script>
+
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h1>Profile</h1>
+  <q-page class="q-pa-lg">
+    <q-list>
+      <!-- ANCHOR - Personal Data -->
+      <q-item
+        clickable
+        class="rounded-borders"
+      >
+        <q-item-section>
+          <q-item-label class="text-h5 text-bold">Дмитрий</q-item-label>
+          <q-item-label caption>Личные данные</q-item-label>
+        </q-item-section>
+
+        <q-item-section avatar>
+          <q-avatar
+            color="grey-4"
+            text-color="white"
+            icon="eva-person"
+            size="5rem"
+          />
+        </q-item-section>
+
+        <q-item-section side>
+          <q-icon name="eva-chevron-right-outline"></q-icon>
+        </q-item-section>
+      </q-item>
+
+      <!-- ANCHOR - Edit Profile -->
+      <q-item
+        clickable
+        class="rounded-borders text-primary"
+      >
+        <q-item-section side>
+          <q-icon
+            color="primary"
+            name="eva-edit-outline"
+          ></q-icon>
+        </q-item-section>
+        <q-item-section>Редактировать профиль</q-item-section>
+      </q-item>
+
+      <q-separator class="q-my-md" />
+
+      <!-- ANCHOR - Cars -->
+      <q-item-label header>Машины</q-item-label>
+
+      <q-item
+        clickable
+        class="rounded-borders text-primary"
+      >
+        <q-item-section side>
+          <q-icon
+            color="primary"
+            name="eva-plus-circle-outline"
+          ></q-icon>
+        </q-item-section>
+        <q-item-section>Добавить авто</q-item-section>
+      </q-item>
+
+      <q-separator class="q-my-md" />
+
+      <!-- ANCHOR - Extra Links -->
+      <q-item
+        clickable
+        v-for="link in extraLinks"
+        :key="link"
+        class="rounded-borders"
+      >
+        <q-item-section>{{ link }}</q-item-section>
+        <q-item-section side>
+          <q-icon name="eva-chevron-right-outline"></q-icon>
+        </q-item-section>
+      </q-item>
+
+      <q-separator class="q-my-md" />
+
+      <!-- ANCHOR - Log Out -->
+      <q-item
+        clickable
+        class="rounded-borders"
+      >
+        <q-item-section class="text-negative">Выйти</q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
-
-<script setup lang="ts">
-</script>
