f781c003dbc72c41dae29e5c625804c84b4bf662
Dmitry_Gorshkov <libafer99@gmail.com>
2024-07-09 14:03:45 +0500

    refactor: выделение карточки предпросмотра поездки в отдельный компонент

---
+++ src/features/TripPreviewCard.vue
@@ -0,0 +1,81 @@
+<script lang="ts" setup>
+import { date as QDate } from 'quasar';
+import { getAvatarURL, ITrip } from 'src/shared/api';
+import { MyAvatar } from 'src/shared/ui';
+
+interface IProps {
+  disabled?: boolean;
+  trip: ITrip;
+}
+
+const props = defineProps<IProps>();
+</script>
+
+<template>
+  <q-item
+    clickable
+    :disable="props.disabled"
+    class="row rounded-borders bg-white shadow-2 q-pa-md"
+  >
+    <div
+      class="column overflow-hidden"
+      style="width: 72px"
+    >
+      <span class="text-subtitle2">{{ trip.departureCity }}</span>
+      <span class="text-caption">{{ QDate.formatDate(trip.departureTime, 'HH:mm') }}</span>
+
+      <q-icon
+        name="eva-more-vertical-outline"
+        class="q-my-sm"
+      />
+
+      <span class="text-subtitle2">{{ trip.arrivalCity }}</span>
+      <span class="text-caption">{{ QDate.formatDate(trip.arrivalTime, 'HH:mm') }}</span>
+    </div>
+
+    <q-separator
+      vertical
+      spaced
+      inset
+    />
+
+    <div class="col column gap-sm">
+      <div class="row justify-between">
+        <my-avatar :src="trip.driver.avatarFileId ? getAvatarURL(trip.driver.avatarFileId) : ''" />
+
+        <div class="col-auto column items-end">
+          <span class="text-bold">{{
+            trip.alreadyReserved === trip.totalPassengers ? 'Мест нет' : trip.price + ' ₽'
+          }}</span>
+          <div class="row items-center gap-xs text-caption">
+            <span>{{ trip.alreadyReserved }}/{{ trip.totalPassengers }}</span>
+            <q-icon name="eva-people-outline" />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="column">
+          <span class="text-subtitle2">{{ trip.driver.name }}</span>
+          <span class="text-caption">{{ trip.driver.cars[0]?.name }}</span>
+        </div>
+      </div>
+
+      <div class="row q-mt-auto gap-sm justify-end">
+        <q-icon
+          v-for="icon in [
+            { name: 'cube', type: 'eva' },
+            { name: 'briefcase', type: 'eva' },
+            { name: 'air', type: 'material' },
+            { name: 'child_care', type: 'material' },
+            { name: 'pets', type: 'material' },
+            { name: 'social_distance', type: 'material' },
+          ]"
+          :key="icon.name"
+          :name="icon.type === 'eva' ? `eva-${icon.name}-outline` : icon.name"
+          size="1.2rem"
+        />
+      </div>
+    </div>
+  </q-item>
+</template>
--- src/pages/trips/list/index.vue
+++ src/pages/trips/list/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { captureApiException } from 'src/shared/utils';
-import { Loading, date as QDate } from 'quasar';
-import { getAllTrips, getAvatarURL, ITrip, Response } from 'src/shared/api';
+import { Loading } from 'quasar';
+import { getAllTrips, ITrip, Response } from 'src/shared/api';
 import PageHeader from './ui/PageHeader.vue';
-import { MyAvatar } from 'src/shared/ui';
+import TripPreviewCard from 'src/features/TripPreviewCard.vue';
 
 const trips = ref<Response<ITrip>[]>([]);
 
@@ -23,75 +23,13 @@ getAllTrips()
         <h5 class="q-mb-sm">Сегодня</h5>
 
         <q-list class="column gap-sm">
-          <q-item
+          <TripPreviewCard
             v-for="trip in trips"
             :key="trip.$id"
-            clickable
-            :disable="trip.alreadyReserved === trip.totalPassengers"
-            class="row rounded-borders bg-white shadow-2 q-pa-md"
+            :trip="trip"
             :to="`/trips/preview/${trip.$id}`"
-          >
-            <div
-              class="column overflow-hidden"
-              style="width: 72px"
-            >
-              <span class="text-subtitle2">{{ trip.departureCity }}</span>
-              <span class="text-caption">{{ QDate.formatDate(trip.departureTime, 'HH:mm') }}</span>
-
-              <q-icon
-                name="eva-more-vertical-outline"
-                class="q-my-sm"
-              />
-
-              <span class="text-subtitle2">{{ trip.arrivalCity }}</span>
-              <span class="text-caption">{{ QDate.formatDate(trip.arrivalTime, 'HH:mm') }}</span>
-            </div>
-
-            <q-separator
-              vertical
-              spaced
-              inset
-            />
-
-            <div class="col column gap-sm">
-              <div class="row justify-between">
-                <my-avatar :src="trip.driver.avatarFileId ? getAvatarURL(trip.driver.avatarFileId) : ''" />
-
-                <div class="col-auto column items-end">
-                  <span class="text-bold">{{
-                    trip.alreadyReserved === trip.totalPassengers ? 'Мест нет' : trip.price + ' ₽'
-                  }}</span>
-                  <div class="row items-center gap-xs text-caption">
-                    <span>{{ trip.alreadyReserved }}/{{ trip.totalPassengers }}</span>
-                    <q-icon name="eva-people-outline" />
-                  </div>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="column">
-                  <span class="text-subtitle2">{{ trip.driver.name }}</span>
-                  <span class="text-caption">{{ trip.driver.cars[0]?.name }}</span>
-                </div>
-              </div>
-
-              <div class="row q-mt-auto gap-sm justify-end">
-                <q-icon
-                  v-for="icon in [
-                    { name: 'cube', type: 'eva' },
-                    { name: 'briefcase', type: 'eva' },
-                    { name: 'air', type: 'material' },
-                    { name: 'child_care', type: 'material' },
-                    { name: 'pets', type: 'material' },
-                    { name: 'social_distance', type: 'material' },
-                  ]"
-                  :key="icon.name"
-                  :name="icon.type === 'eva' ? `eva-${icon.name}-outline` : icon.name"
-                  size="1.2rem"
-                />
-              </div>
-            </div>
-          </q-item>
+            :disabled="trip.alreadyReserved === trip.totalPassengers"
+          />
         </q-list>
       </q-page>
     </q-page-container>
