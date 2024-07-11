b75e37514a5ddab6109f6a2970be56ba02d779a9
nogavadu <dkuets@gmail.com>
2024-07-11 17:40:41 +0300

    refactor: декомпозиция update-car

---
+++ src/features/UpdateCar/ui/CarBodyTypeStep.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { MyItem } from 'src/shared/ui';
+
+defineProps<{
+  currentBodyType: string
+}>();
+
+const bodyType = defineModel({ default: '' });
+const emit = defineEmits(['click']);
+const handleBodyTypeChoose = (type: string) => {
+  bodyType.value = type;
+  emit('click');
+};
+</script>
+
+<template>
+  <div>
+    <h4 class="q-ma-lg">Какой тип кузова у вашего авто?</h4>
+
+    <q-list class="q-my-sm">
+      <!-- TODO: Icons of body types for items -->
+      <my-item
+        v-for="type in ['Седан', 'Хэтчбэк', 'Легковой фургон', 'Универсал', 'Кроссовер', 'Минивэн']"
+        :key="type"
+        clickable
+        chevron
+        :label="type"
+        :active="type === currentBodyType"
+        class="q-mx-sm"
+        @click="handleBodyTypeChoose(type)"
+      />
+    </q-list>
+  </div>
+</template>
---
+++ src/features/UpdateCar/ui/CarColorStep.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { CAR_COLORS } from 'src/shared/constants';
+
+defineProps<{
+  currentColor: string
+}>();
+
+const carColor = defineModel({ default: '' });
+const emit = defineEmits(['click']);
+
+
+const handleColorChoose = (color: string) => {
+  carColor.value = color;
+  emit('click');
+};
+</script>
+
+<template>
+  <div>
+    <h4 class="q-ma-lg">Какого цвета ваша машина?</h4>
+
+    <q-list>
+      <q-item
+        v-for="item in CAR_COLORS"
+        :key="item.color"
+        clickable
+        class="rounded-borders q-mx-sm"
+        :active="item.color === currentColor"
+        @click="handleColorChoose(item.color)"
+      >
+        <q-item-section side>
+          <q-icon
+            name="circle"
+            :color="item.value"
+          />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ item.color }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-icon name="eva-chevron-right-outline"></q-icon>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
