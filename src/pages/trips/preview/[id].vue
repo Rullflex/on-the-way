b977be3a9e99643e1815f0f426c3c597f2870d6b
Dmitry_Gorshkov <libafer99@gmail.com>
2024-07-15 12:02:35 +0500

    refactor: изменение лейаута на странице просмотра поездки

--- src/pages/trips/preview/[id].vue
+++ src/pages/trips/preview/[id].vue
@@ -32,150 +32,139 @@ getTripById(props.id)
 </script>
 
 <template>
-  <q-layout>
-    <q-page-container>
-      <q-page
-        v-if="trip"
-        class="q-pb-lg"
+  <q-page v-if="trip">
+    <div class="q-px-lg">
+      <h5 class="q-mb-lg">{{ tripDate }}</h5>
+
+      <div class="column">
+        <span class="text-bold">{{ trip.canPickUpFromPlace ? 'Заберу с места' : trip.departureAddress }}</span>
+        <span class="text-subtitle2">{{ trip.departureCity }}</span>
+        <span class="text-subtitle2">{{ trip.departureTime }}</span>
+
+        <q-icon
+          name="eva-more-vertical-outline"
+          class="q-my-sm"
+        />
+
+        <span class="text-bold">{{ trip.canDriveToPlace ? 'Довезу до места' : trip.arrivalAddress }}</span>
+        <span class="text-subtitle2">{{ trip.arrivalCity }}</span>
+      </div>
+    </div>
+
+    <q-separator
+      spaced="1rem"
+      size="0.5rem"
+    />
+
+    <div class="q-px-lg">
+      <span class="text-h6">{{ trip.price }} ₽</span> за 1 пассажира, осталось
+      {{ trip.totalPassengers - trip.alreadyReserved }}
+      {{ getPluralNoun(trip.totalPassengers - trip.alreadyReserved, 'место', 'места', 'мест') }}
+    </div>
+
+    <q-separator
+      spaced="1rem"
+      size="0.5rem"
+    />
+
+    <q-list class="q-px-sm">
+      <my-item
+        chevron
+        :label="trip.driver.name"
+        :to="`/profile/preview/${trip.driver.$id}`"
       >
-        <div class="sticky-top bg-white q-pa-md z-top">
-          <my-back-btn fallback-route="/trips/list" />
-        </div>
+        <template #append>
+          <my-avatar :src="trip.driver.avatarFileId ? getAvatarURL(trip.driver.avatarFileId) : ''" />
+        </template>
+      </my-item>
 
-        <div class="q-px-lg">
-          <h5 class="q-mb-lg">{{ tripDate }}</h5>
+      <q-item class="q-my-sm">
+        <q-item-section>
+          <q-btn
+            dense
+            outline
+            size="0.8rem"
+            padding="6px"
+            label="Позвонить"
+            icon="eva-phone-outline"
+            :href="`tel:${trip.driver.phone}`"
+          />
+        </q-item-section>
 
-          <div class="column">
-            <span class="text-bold">{{ trip.canPickUpFromPlace ? 'Заберу с места' : trip.departureAddress }}</span>
-            <span class="text-subtitle2">{{ trip.departureCity }}</span>
-            <span class="text-subtitle2">{{ trip.departureTime }}</span>
+        <q-item-section>
+          <q-btn
+            dense
+            outline
+            size="0.8rem"
+            padding="6px"
+            label="Написать"
+            icon="eva-message-circle-outline"
+          />
+        </q-item-section>
+      </q-item>
 
-            <q-icon
-              name="eva-more-vertical-outline"
-              class="q-my-sm"
+      <q-item>
+        <q-item-section>
+          <q-item-label caption>Комментарий</q-item-label>
+          <q-item-label>{{ trip.comment }}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator
+        spaced
+        inset
+      />
+
+      <q-item v-if="trip.car">
+        <q-item-section side>
+          <q-icon name="eva-car-outline" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ trip.car.name }}</q-item-label>
+          <q-item-label caption>
+            {{ trip.car.year }}, {{ trip.car.color }}
+            <q-chip
+              v-if="trip.car.licensePlate"
+              dense
+              :label="trip.car.licensePlate"
             />
+          </q-item-label>
+        </q-item-section>
+      </q-item>
 
-            <span class="text-bold">{{ trip.canDriveToPlace ? 'Довезу до места' : trip.arrivalAddress }}</span>
-            <span class="text-subtitle2">{{ trip.arrivalCity }}</span>
-          </div>
-        </div>
+      <q-separator
+        spaced
+        inset
+      />
 
-        <q-separator
-          spaced="1rem"
-          size="0.5rem"
-        />
+      <my-item
+        v-for="convenience in tripConveniences"
+        :key="convenience?.name"
+        :icon="convenience?.icon"
+        :label="convenience?.title"
+      />
+    </q-list>
 
-        <div class="q-px-lg">
-          <span class="text-h6">{{ trip.price }} ₽</span> за 1 пассажира, осталось
-          {{ trip.totalPassengers - trip.alreadyReserved }}
-          {{ getPluralNoun(trip.totalPassengers - trip.alreadyReserved, 'место', 'места', 'мест') }}
-        </div>
+    <q-separator
+      spaced="1rem"
+      size="0.5rem"
+    />
 
-        <q-separator
-          spaced="1rem"
-          size="0.5rem"
-        />
+    <q-list class="q-px-sm">
+      <my-item
+        color="primary"
+        label="Поделиться поездкой"
+      />
 
-        <q-list class="q-px-sm">
-          <my-item
-            chevron
-            :label="trip.driver.name"
-            :to="`/profile/preview/${trip.driver.$id}`"
-          >
-            <template #append>
-              <my-avatar :src="trip.driver.avatarFileId ? getAvatarURL(trip.driver.avatarFileId) : ''" />
-            </template>
-          </my-item>
-
-          <q-item class="q-my-sm">
-            <q-item-section>
-              <q-btn
-                dense
-                outline
-                size="0.8rem"
-                padding="6px"
-                label="Позвонить"
-                icon="eva-phone-outline"
-                :href="`tel:${trip.driver.phone}`"
-              />
-            </q-item-section>
-
-            <q-item-section>
-              <q-btn
-                dense
-                outline
-                size="0.8rem"
-                padding="6px"
-                label="Написать"
-                icon="eva-message-circle-outline"
-              />
-            </q-item-section>
-          </q-item>
-
-          <q-item>
-            <q-item-section>
-              <q-item-label caption>Комментарий</q-item-label>
-              <q-item-label>{{ trip.comment }}</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator
-            spaced
-            inset
-          />
-
-          <q-item v-if="trip.car">
-            <q-item-section side>
-              <q-icon name="eva-car-outline" />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ trip.car.name }}</q-item-label>
-              <q-item-label caption>
-                {{ trip.car.year }}, {{ trip.car.color }}
-                <q-chip
-                  v-if="trip.car.licensePlate"
-                  dense
-                  :label="trip.car.licensePlate"
-                />
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator
-            spaced
-            inset
-          />
-
-          <my-item
-            v-for="convenience in tripConveniences"
-            :key="convenience?.name"
-            :icon="convenience?.icon"
-            :label="convenience?.title"
-          />
-        </q-list>
-
-        <q-separator
-          spaced="1rem"
-          size="0.5rem"
-        />
-
-        <q-list class="q-px-sm">
-          <my-item
-            color="primary"
-            label="Поделиться поездкой"
-          />
-
-          <my-item
-            color="primary"
-            label="Пожаловаться на поездку"
-          />
-        </q-list>
-      </q-page>
-    </q-page-container>
-  </q-layout>
+      <my-item
+        color="primary"
+        label="Пожаловаться на поездку"
+      />
+    </q-list>
+  </q-page>
 </template>
 
 <route lang="yaml">
 meta:
-  layout: blank
+  layout: subPage
 </route>
