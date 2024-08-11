c793f1a4e6100aa4e9d430822b326821ab83600f
Dmitry_Gorshkov <libafer99@gmail.com>
2024-08-11 19:21:43 +0500

    chore: 404 page

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-secondary text-white text-center q-pa-md flex flex-center">
     <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
+      <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+      <div
+        class="text-h3"
+        style="opacity: 0.4"
+      >
+        Упс. Здесь ничего нет...
       </div>
 
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="primary"
         unelevated
         to="/"
-        label="Go Home"
+        label="На главную"
         no-caps
       />
     </div>
@@ -24,6 +25,6 @@
 
 <script setup lang="ts">
 defineOptions({
-  name: 'ErrorNotFound'
+  name: 'ErrorNotFound',
 });
 </script>
