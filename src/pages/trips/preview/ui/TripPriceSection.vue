3bf2e1dcdba5f8b3b95b088e9a43f6413015bb08
Dmitry_Gorshkov <libafer99@gmail.com>
2024-08-20 14:15:22 +0500

    feat: отображение цены поездки за кол-во мест

--- src/pages/trips/preview/ui/TripPriceSection.vue
+++ src/pages/trips/preview/ui/TripPriceSection.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import { getPluralNoun } from 'src/shared/utils';
 import { ITrip } from 'src/shared/types';
+import { useTripSettingsStore } from 'src/stores/trip-settings';
+
+const tripSettingsStore = useTripSettingsStore();
 
 defineProps<{
-  trip: ITrip
+  trip: ITrip;
 }>();
 </script>
 
 <template>
   <div class="q-px-lg">
-    <span class="text-h6">{{ trip.price }} ₽</span> за 1 пассажира, осталось
+    <span class="text-h6">{{ trip.price * tripSettingsStore.passengers }} ₽</span> за
+    {{ tripSettingsStore.passengers }}
+    {{ getPluralNoun(tripSettingsStore.passengers, 'пассажира', 'пассажиров', 'пассажиров') }}, осталось
     {{ trip.totalPassengers - trip.passengerIds.length }}
     {{ getPluralNoun(trip.totalPassengers - trip.passengerIds.length, 'место', 'места', 'мест') }}
   </div>
