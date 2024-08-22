57f075ccd3a16a67d248d13d1e045b9baa20fb50
Dmitry_Gorshkov <libafer99@gmail.com>
2024-08-22 11:17:43 +0500

    feat: Новый стиль для лоадера

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
 </template>
 
 <script setup lang="ts">
+import { Loading, QSpinnerPuff } from 'quasar';
+
+Loading.setDefaults({
+  spinner: QSpinnerPuff,
+  spinnerColor: 'primary',
+  backgroundColor: 'grey-3',
+});
+
 onMounted(() => {
   document.body.classList.remove('app-loading');
 });
