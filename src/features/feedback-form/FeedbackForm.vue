9a2d5a3e4c8f1ee26c628c3877400d713e6aecd8
Dmitry_Gorshkov <libafer99@gmail.com>
2024-08-23 15:05:58 +0500

    feat: отправка почты

--- src/features/feedback-form/FeedbackForm.vue
+++ src/features/feedback-form/FeedbackForm.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
 import { Notify } from 'quasar';
+import { useUserStore } from 'src/stores/user';
 
 const text = ref('');
+const isPending = ref(false);
+const user = useUserStore();
 
 const props = withDefaults(
   defineProps<{
@@ -14,15 +17,34 @@ const props = withDefaults(
   }
 );
 
-const onSubmit = () => {
-  // TODO: Отправка отзыва на почту
+const onSubmit = async () => {
+  const body = new FormData();
+  body.append('text', text.value);
+  body.append('title', 'Отзыв с формы обратной связи');
+  body.append('info', JSON.stringify(user.$state));
 
-  // TODO: вместо уведомления заменить контент на сообщение и картинку об успешной отправке
-  Notify.create({
-    type: 'positive',
-    message: props.successMessage,
-    position: 'top',
-  });
+  try {
+    isPending.value = true;
+    await fetch('/mail.php', {
+      method: 'POST',
+      body,
+    });
+
+    // TODO: вместо уведомления заменить контент на сообщение и картинку об успешной отправке
+    Notify.create({
+      type: 'positive',
+      message: props.successMessage,
+      position: 'top',
+    });
+  } catch (error) {
+    Notify.create({
+      type: 'negative',
+      message: 'Произошла ошибка. Повторите позже',
+      position: 'top',
+    });
+  } finally {
+    isPending.value = false;
+  }
 };
 </script>
 
@@ -42,6 +64,7 @@ const onSubmit = () => {
     />
 
     <q-btn
+      :loading="isPending"
       unelevated
       color="primary"
       type="submit"
